98dcf305d23d63278c341042b4d3e9fe9cae4bd6
Ian Engelbrecht <ianicus.za@gmail.com>
2025-04-02 12:59:47 +0200

    updated sw version

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "geo-recorder-cache-v1";
+const CACHE_NAME = "geo-recorder-cache-v2";
 const ASSETS = [
   ".",
   "index.html",
@@ -16,5 +16,13 @@ self.addEventListener("install", (event) => {
 self.addEventListener("fetch", (event) => {
   event.respondWith(
     caches.match(event.request).then((response) => response || fetch(event.request))
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key)))
+    )
   );
 });
