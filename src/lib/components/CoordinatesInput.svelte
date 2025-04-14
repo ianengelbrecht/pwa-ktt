8dab3780d02a18a63ecab2632aa807d83b0fd913
Ian Engelbrecht <ianicus.za@gmail.com>
2025-04-14 16:25:30 +0200

    added coordinates input and workshop

---
+++ src/lib/components/CoordinatesInput.svelte
@@ -0,0 +1,94 @@
+<script lang="ts">
+  import { convert } from 'geo-coordinates-parser';
+
+  let placeHolderCoordinates = $state('Add coordinates');
+  let coordinates: string | null = $state(null);
+  let decimalCoordinates: string | null = $state(null);
+  let accuracy: number | null = $state(null);
+  let coordinatesError = $state(false);
+
+  if (navigator.geolocation) {
+    navigator.geolocation.watchPosition(
+      (position) => {
+        const { latitude, longitude } = position.coords;
+        placeHolderCoordinates = `${latitude.toFixed(6)}, ${longitude.toFixed(6)}`;
+        accuracy = Number(position.coords.accuracy.toFixed(0));
+      },
+      (error) => {
+        console.error('Error getting location:', error);
+      },
+      {
+        enableHighAccuracy: true,
+        maximumAge: 0,
+        timeout: 5000,
+      },
+    );
+  } else {
+    placeHolderCoordinates = 'Add coordinates';
+  }
+
+  const handleCoordinatesChange = (ev: Event) => {
+    if (coordinates) {
+      try {
+        const converted = convert(coordinates);
+        decimalCoordinates = converted.decimalCoordinates;
+        accuracy = null;
+      } catch (error) {
+        console.error('Error converting coordinates:', error);
+        coordinatesError = true;
+      }
+    }
+  };
+
+  const updateAccuracy = () => {
+    accuracy = 50;
+  };
+</script>
+
+<div class="relative w-full"></div>
+{#if decimalCoordinates}
+  <span class="text-xs text-gray-500">{decimalCoordinates}</span>
+{/if}
+{#if coordinatesError}
+  <span class="text-xs text-red-600">Invalid coordinates</span>
+{/if}
+<div
+  class={[
+    'flex gap-2 w-full items-center rounded border',
+    {
+      'ring-2 ring-offset-2 ring-offset-black ring-orange-300':
+        accuracy && accuracy > 20 && accuracy < 50,
+    },
+    ,
+    {
+      'ring-3 ring-offset-2 ring-offset-black ring-red-600':
+        (accuracy && accuracy >= 50) || coordinatesError,
+    },
+  ]}
+>
+  <div class="flex-1 relative">
+    <input
+      type="text"
+      class="w-full input-base"
+      onpaste={handleCoordinatesChange}
+      onblur={handleCoordinatesChange}
+      bind:value={coordinates}
+      placeholder={placeHolderCoordinates}
+    />
+  </div>
+  {#if accuracy}
+    <span
+      class={[
+        'min-w-fit',
+        {
+          'text-orange-300': accuracy && accuracy > 20 && accuracy < 50,
+        },
+        { 'text-red-600': accuracy && accuracy >= 50 },
+      ]}
+    >
+      ± {accuracy}m&nbsp;
+    </span>
+  {/if}
+</div>
+
+<button class="btn mt-4" onclick={updateAccuracy}>Add 50m uncertainty</button>
---
+++ src/routes/workshop/+page.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+  import CoordinatesInput from '$lib/components/CoordinatesInput.svelte';
+</script>
+
+<div class="p-4 w-1/3">
+  <p>This is a coordinates input</p>
+  <CoordinatesInput />
+</div>
